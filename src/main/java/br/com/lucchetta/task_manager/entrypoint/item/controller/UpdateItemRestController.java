4b94aa7b0da618429213ee83729bc4dca0e4a240
Alexandre Luchetti <luchetti.92@gmail.com>
2024-08-23 18:46:45 -0300

    Refatorando a clean archtecture

---
+++ src/main/java/br/com/lucchetta/task_manager/entrypoint/item/controller/UpdateItemRestController.java
@@ -0,0 +1,52 @@
+package br.com.lucchetta.task_manager.entrypoint.item.controller;
+
+import br.com.lucchetta.task_manager.configuration.exception.NoneResultException;
+import br.com.lucchetta.task_manager.core.entity.Item;
+import br.com.lucchetta.task_manager.core.entity.Lista;
+import br.com.lucchetta.task_manager.core.useCase.item.ItemUseCase;
+import br.com.lucchetta.task_manager.core.useCase.lista.ListaUseCase;
+import br.com.lucchetta.task_manager.entrypoint.item.dto.ItemDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+
+import static br.com.lucchetta.task_manager.entrypoint.item.ItemRequestPath.*;
+
+@RestController
+@RequestMapping(ITEM_PATH)
+@Tag(name = ITEM_TAG, description = ITEM_DESCRIPTION)
+public class UpdateItemRestController {
+
+    private final ItemUseCase itemUseCase;
+    private final ListaUseCase listaUseCase;
+
+    @Autowired
+    public UpdateItemRestController(ItemUseCase itemUseCase, ListaUseCase listaUseCase) {
+        this.itemUseCase = itemUseCase;
+        this.listaUseCase = listaUseCase;
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Atualizar um item", description = "Retorna o item, de acordo com o id informado, com os dados atualizados")
+    public ResponseEntity<Item> updateItem(@PathVariable Long listaId, @PathVariable Long id, @RequestBody ItemDto dto) {
+        Item item = getItemFromLista(listaId, id);
+        if (item == null) throw new NoneResultException("Nenhum item encontrado com o id informado");
+
+        Lista lista = listaUseCase.findById(listaId);
+        item.update(dto, lista);
+        return ResponseEntity.ok(itemUseCase.save(item));
+    }
+
+    private Item getItemFromLista(Long listaId, Long id) {
+        List<Item> itens = itemUseCase.findAllByListaId(listaId);
+        for (Item item : itens) {
+            if (Objects.equals(item.getId(), id)) return item;
+        }
+        return null;
+    }
+}
---
+++ src/main/java/br/com/lucchetta/task_manager/entrypoint/lista/controller/CreateListRestController.java
@@ -0,0 +1,34 @@
+package br.com.lucchetta.task_manager.entrypoint.lista.controller;
+
+import br.com.lucchetta.task_manager.core.entity.Lista;
+import br.com.lucchetta.task_manager.core.useCase.lista.ListaUseCase;
+import br.com.lucchetta.task_manager.entrypoint.lista.dto.ListaDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static br.com.lucchetta.task_manager.entrypoint.lista.ListaRequestPath.*;
+
+@RestController
+@RequestMapping(LISTA_PATH)
+@Tag(name = LISTA_TAG, description = LISTA_DESCRIPTION)
+public class CreateListRestController {
+
+    private final ListaUseCase listaUseCase;
+
+    @Autowired
+    public CreateListRestController(ListaUseCase listaUseCase) {
+        this.listaUseCase = listaUseCase;
+    }
+
+    @PostMapping
+    @Operation(summary = "Cria uma lista", description = "Retorna a lista criada")
+    public Lista createLista(@RequestBody ListaDto dto) {
+        Lista lista = dto.toObject();
+        return listaUseCase.save(lista);
+    }
+}
